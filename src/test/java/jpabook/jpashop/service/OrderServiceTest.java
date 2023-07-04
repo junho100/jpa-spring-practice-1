21e8437f0b5c485649d40fff1542a44a5f511ed9
junho100 <bemodesty306@naver.com>
2023-07-05 01:42:46 +0900

    section 6

---
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,62 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+
+@SpringBootTest
+@Transactional
+class OrderServiceTest {
+
+    @Autowired
+    EntityManager em;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void 상품주문() throws Exception {
+        Member member = new Member();
+        member.setName("user1");
+        member.setAddress(new Address("seoul", "gang-ga", "123-123"));
+        em.persist(member);
+
+        Item book = new Book();
+        book.setName("JPA");
+        book.setPrice(10000);
+        book.setStockQuantity(10);
+        em.persist(book);
+
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals("ORDER", OrderStatus.ORDER, getOrder.getStatus());
+        assertEquals("accurate item count", 1, getOrder.getOrderItems().size());
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+
+    }
+
+    @Test
+    public void 상품주문_재고수량초과() throws Exception {
+
+    }
+}
